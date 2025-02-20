77fdd6d51b215d76030566ebb05154a37ea6aafb
dhanavel10 <svdhanavel10@gmail.com>
2025-02-20 09:32:34 +0530

    Update Main.java

--- BankingApplication/src/com/application/pack1/Main.java
+++ BankingApplication/src/com/application/pack1/Main.java
@@ -7,7 +7,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-	int c=0;
 	
 	public static void main(String []args) {
 		
@@ -39,34 +38,22 @@ public class Main {
 			functions.logIn();
 			break;
 		case 3:
-			if(c==1) {
+
 			functions.deposit();
 			System.out.println("Deposit succesfull");
-			}
 			break;
 			
 		case 4:
-			if(c==1) {
-				functions.withDraw();
-			}
+			functions.withDraw();
 			break;
 		case 5:
-			if(c==1) {
-				functions.checkBalance();
-			}
-			break;
-			
+			functions.checkBalance();
+			break;	
 		case 6:
-			if(c==1) {
-				functions.tranferAmount();
-			}
+			functions.tranferAmount();
 			break;
-			
 		case 7:
-			if(c==1) {
-				functions.logOut();
-				c=0;
-			}
+			functions.logOut();
 			break;
 		case 8:
 			System.out.println("Process end");
